46bff622156003cdf1e7aa9529a80631432f98da
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-23 16:34:52 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -1,7 +1,7 @@
 # Create a zip file with function code
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "./index.mjs"
+  source_file = "modules/lambda/index.mjs"
   output_path = "lambda.zip"
 }
 
